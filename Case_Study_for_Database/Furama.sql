b1fb337b4761a0c968cac8d8f56a86ac15bcec54
trinhminhduc9x <duc1997th@gmail.com>
2022-07-13 17:20:18 +0700

    add

--- Case_Study_for_Database/Furama.sql
+++ Case_Study_for_Database/Furama.sql
@@ -1,56 +1,64 @@
 CREATE DATABASE CaseStudyforDatabase;
 USE CaseStudyforDatabase;
+
 CREATE TABLE nhan_vien (
-    ma_nhan_vien INT AUTO_INCREMENT NOT NULL,
-    ho_ten VARCHAR(45) NOT NULL,
-    ngay_sinh DATE NOT NULL,
-    so_cmnd VARCHAR(45) NOT NULL,
-    luong DOUBLE NOT NULL,
-    so_dien_thoai VARCHAR(45) NOT NULL,
-    email VARCHAR(45),
-    dia_chi VARCHAR(45),
-    ma_vi_tri INT,
-    ma_trinh_do INT,
-    ma_bo_phan INT,
-    PRIMARY KEY (ma_nhan_vien)
+    ma_nhan_vien 	INT 				NOT NULL 	AUTO_INCREMENT PRIMARY KEY,
+    ho_ten 			VARCHAR(45) 		NOT NULL,
+    ngay_sinh 		DATE 				NOT NULL,
+    so_cmnd 		VARCHAR(45) 		NOT NULL 	UNIQUE,
+    luong 			DOUBLE 				NOT NULL,
+    so_dien_thoai 	VARCHAR(45) 		NOT NULL 	UNIQUE,
+    email 			VARCHAR(45),
+    dia_chi 		VARCHAR(45),
+    ma_vi_tri 		INT,
+    ma_trinh_do 	INT,
+    ma_bo_phan 		INT,
+    FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri(ma_vi_tri),
+    FOREIGN KEY ( ma_trinh_do)
+        REFERENCES  trinh_do ( ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
 );
 
-CREATE TABLE vi_tri (
-    ma_vi_tri INT AUTO_INCREMENT NOT NULL,
-    ten_vi_tri VARCHAR(45) NOT NULL,
+CREATE TABLE vi_tri(
+    ma_vi_tri 		INT 				NOT NULL	AUTO_INCREMENT,
+    ten_vi_tri 		VARCHAR(45)			NOT NULL,
     PRIMARY KEY (ma_vi_tri)
 );
 
 CREATE TABLE trinh_do (
-    ma_trinh_do INT AUTO_INCREMENT NOT NULL,
-    ten_trinh_do VARCHAR(45) NOT NULL,
+    ma_trinh_do 	INT  				NOT NULL 	AUTO_INCREMENT,
+    ten_trinh_do 	VARCHAR(45) NOT NULL,
     PRIMARY KEY (ma_trinh_do)
 );
 
 CREATE TABLE bo_phan (
-    ma_bo_phan INT AUTO_INCREMENT NOT NULL,
-    ten_bo_phan VARCHAR(45) NOT NULL,
+    ma_bo_phan 		INT  				NOT NULL 	AUTO_INCREMENT,
+    ten_bo_phan 	VARCHAR(45) NOT NULL,
     PRIMARY KEY (ma_bo_phan)
 );
 
 
 
 CREATE TABLE khach_hang (
-    ma_khach_hang INT AUTO_INCREMENT NOT NULL,
-    ma_loai_khach INT AUTO_INCREMENT NOT NULL,
-    ho_ten VARCHAR(45) NOT NULL,
-    ngay_sinh DATE NOT NULL,
-    gioi_tinh BIT NOT NULL,
-    so_cmnd VARCHAR(45) NOT NULL,
-    so_dien_thoai VARCHAR(45) NOT NULL,
-    email VARCHAR(45),
-    dia_chi VARCHAR(45),
-    PRIMARY KEY (ma_khach_hang)
+    ma_khach_hang 	INT 				NOT NULL	AUTO_INCREMENT,
+    ma_loai_khach 	INT 				NOT NULL,
+    ho_ten 			VARCHAR(45) 		NOT NULL,
+    ngay_sinh 		DATE 				NOT NULL,
+    gioi_tinh 		BIT 				NOT NULL,
+    so_cmnd 		VARCHAR(45) 		NOT NULL,
+    so_dien_thoai 	VARCHAR(45) 		NOT NULL,
+    email 			VARCHAR(45),
+    dia_chi 		VARCHAR(45),
+    PRIMARY KEY (ma_khach_hang),
+     FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach)
 );
 
 CREATE TABLE loai_khach (
-    ma_loai_khach INT AUTO_INCREMENT NOT NULL,
-    ten_loai_khach VARCHAR(45) NOT NULL,
+    ma_loai_khach 	INT 				NOT NULL 	AUTO_INCREMENT,
+    ten_loai_khach 	VARCHAR(45) 		NOT NULL,
     PRIMARY KEY (ma_loai_khach)
 );
 
@@ -60,65 +68,78 @@ CREATE TABLE loai_khach (
 
 
 CREATE TABLE dich_vu (
-    ma_dich_vu INT AUTO_INCREMENT NOT NULL,
-    ten_dich_vu VARCHAR(45) NOT NULL,
-    dien_tich INT,
-    chi_phi_thue DATETIME NOT NULL,
-    tien_dat_coc DOUBLE NOT NULL,
+    ma_dich_vu 		INT  				NOT NULL 	AUTO_INCREMENT,
+    ten_dich_vu 	VARCHAR(45) 		NOT NULL,
+    dien_tich 		INT,
+    chi_phi_thue 	DATETIME 			NOT NULL,
+    tien_dat_coc 	DOUBLE 				NOT NULL,
     so_nguoi_toi_da INT,
-    ma_kieu_thue INT,
+    ma_kieu_thue 	INT,
     ma_loai_dich_vu INT,
     tieu_chuan_phong VARCHAR(45),
     mo_ta_tien_nghi_khac VARCHAR(45),
     dien_tich_ho_boi DOUBLE,
-    so_tang INT,
+    so_tang 		INT,
     dich_vu_mien_phi_di_kem TEXT,
-    PRIMARY KEY (ma_dich_vu)
+    PRIMARY KEY (ma_dich_vu),
+      FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+	  FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue)
 );
 
 
 
 CREATE TABLE loai_dich_vu (
-    ma_loai_dich_vu INT AUTO_INCREMENT NOT NULL,
-    ten_loai_dich_vu VARCHAR(45) NOT NULL,
+    ma_loai_dich_vu INT 				NOT NULL 	AUTO_INCREMENT ,
+    ten_loai_dich_vu VARCHAR(45) 		NOT NULL,
     PRIMARY KEY (ma_loai_dich_vu)
 );
 
 
 CREATE TABLE kieu_thue (
-    ma_kieu_thue INT AUTO_INCREMENT NOT NULL,
+    ma_kieu_thue 	INT  				NOT NULL 	AUTO_INCREMENT,
     ten_kieu_thue VARCHAR(45) NOT NULL,
     PRIMARY KEY (ma_kieu_thue)
 );
 
 
 CREATE TABLE hop_dong (
-    ma_hop_dong INT AUTO_INCREMENT NOT NULL,
-    ngay_lam_hop_dong DATETIME NOT NULL,
-    ngay_ket_thuc DATETIME NOT NULL,
-    tien_dat_coc DOUBLE NOT NULL,
-    ma_nhan_vien INT NOT NULL,
-    ma_khach_hang INT NOT NULL,
-    ma_dich_vu INT NOT NULL,
-    PRIMARY KEY (ma_hop_dong)
+    ma_hop_dong 	INT 				AUTO_INCREMENT NOT NULL,
+    ngay_lam_hop_dong DATETIME 			NOT NULL,
+    ngay_ket_thuc DATETIME				NOT NULL,
+    tien_dat_coc DOUBLE 				NOT NULL,
+    ma_nhan_vien INT 					NOT NULL,
+    ma_khach_hang INT 					NOT NULL,
+    ma_dich_vu INT 						NOT NULL,
+    PRIMARY KEY (ma_hop_dong),
+     FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang),
+	FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien),
+	FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
+
 );
 
-CREATE TABLE hop_dong_chi_tiet (
-    ma_hop_dong_chi_tiet INT AUTO_INCREMENT NOT NULL,
-    ma_hop_dong INT NOT NULL,
-    ma_dich_vu_di_kem INT NOT NULL,
-    mso_luong INT NOT NULL,
-    PRIMARY KEY (ma_hop_dong_chi_tiet)
+CREATE TABLE hop_dong_chi_tiet(
+    ma_hop_dong_chi_tiet 	INT 	AUTO_INCREMENT 		NOT NULL,
+    ma_hop_dong 			INT 	NOT NULL,
+    ma_dich_vu_di_kem 		INT 	NOT NULL,
+    so_luong 				INT 	NOT NULL,
+    PRIMARY KEY (ma_hop_dong_chi_tiet),
+	FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+	FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem)
 );
 
 CREATE TABLE dich_vu_di_kem (
-    ma_dich_vu_di_kem INT AUTO_INCREMENT NOT NULL,
-    ten_dich_vu_di_kem VARCHAR(45) NOT NULL,
-    gia DOUBLE NOT NULL,
-    don_vi VARCHAR(45) NOT NULL,
-    trang_thai VARCHAR(45) NOT NULL,
+    ma_dich_vu_di_kem 		INT 			AUTO_INCREMENT NOT NULL,
+    ten_dich_vu_di_kem 		VARCHAR(45) 	NOT NULL,
+    gia 					DOUBLE 			NOT NULL,
+    don_vi 					VARCHAR(45) 	NOT NULL,
+    trang_thai 				VARCHAR(45) 	NOT NULL,
     PRIMARY KEY (ma_dich_vu_di_kem)
 );
-
-
 
