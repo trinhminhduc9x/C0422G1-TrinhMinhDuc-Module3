038572b2a949f0d37c6aa5308575d9a68fe11884
trinhminhduc9x <duc1997th@gmail.com>
2022-07-18 20:53:38 +0700

    Add

--- ss4_Cac_ham_thong_dung_trong_SQL/exercise/baitap.sql
+++ ss4_Cac_ham_thong_dung_trong_SQL/exercise/baitap.sql
@@ -1,23 +1,36 @@
-USE QuanLySinhVien;
--- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-
-SELECT SubName, MAX(Credit) as max
-FROM Subject
-GROUP BY SubName 
-order by max desc
-;
--- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-SELECT SubName, MAX(Credit) as max
-FROM Subject
-GROUP BY SubName ;
-
--- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-
+-- 1. Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 SELECT 
-    S.StudentId, S.StudentName, AVG(Mark) AS avg
+    *
 FROM
-    Student S
+    subject s
+WHERE
+    s.credit = (SELECT 
+            MAX(credit)
+        FROM
+            subject);
+            
+            
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+SELECT 
+    s.*, m.mark
+FROM
+    subject s
         JOIN
-    Mark M ON S.StudentId = M.StudentId
-GROUP BY S.StudentId , S.StudentName
-ORDER BY avg DESC;
+    mark m ON m.sub_id = s.sub_id
+WHERE
+    m.mark = (SELECT 
+            MAX(mark)
+        FROM
+            mark);
+            
+            
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+SELECT 
+    s.*, AVG(m.mark) AS diem_trung_binh
+FROM
+    student s
+        LEFT JOIN
+    mark m ON m.student_id = s.student_id
+GROUP BY m.student_id
+ORDER BY diem_trung_binh DESC;
+		
