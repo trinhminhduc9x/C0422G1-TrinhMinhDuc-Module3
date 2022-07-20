09db5fc820b1e47a4278e02d21dee49ca7a11bcd
trinhminhduc9x <duc1997th@gmail.com>
2022-07-20 22:12:54 +0700

    add

--- Case_Study_for_Database/Furama-1-10.sql
+++ Case_Study_for_Database/Furama-1-10.sql
@@ -1,7 +1,11 @@
 USE CaseStudyforDatabase;
 
 -- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+
+
 -- c1
+
+
 SELECT 
   *
 FROM
